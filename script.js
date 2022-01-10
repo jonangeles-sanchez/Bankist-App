1d3154022a681fc34c0d6331134da9d52fd0729e
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2022-01-10 17:02:44 -0500

    HTML/CSS Update
    Flow chart, images, and styles includes

--- script.js
+++ script.js
@@ -1,40 +1,76 @@
 'use strict';
-const poll = {
-  question: 'What is your favorite programming language?',
-  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
 
-  answers: new Array(4).fill(0),
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
 };
 
-poll.registerNewAnswer = document
-  .querySelector('.poll')
-  .addEventListener('click', function () {
-    let answer = -1;
-    while (answer > 3 || answer < 0) {
-      answer = prompt(
-        'What is your favorite programming language?\n0: JavaScript\n1: Python\n2: Rust\n3: C++\n(write option number)'
-      );
-    }
-    if (answer == 0) {
-      poll.answers[0]++;
-    } else if (answer == 1) {
-      poll.answers[1]++;
-    } else if (answer == 2) {
-      poll.answers[2]++;
-    } else if (answer == 3) {
-      poll.answers[3]++;
-    }
-    //console.log(answer);
-    poll.displayResults();
-    //poll.displayResults('string');
-    //poll.displayResults('array');
-  });
-
-poll.displayResults = function () {
-  let type = prompt('Enter type\nString or array.');
-  if (type === 'array') {
-    console.log(this.answers);
-  } else if (type == 'string') {
-    console.log(...this.answers);
-  }
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
 };
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
